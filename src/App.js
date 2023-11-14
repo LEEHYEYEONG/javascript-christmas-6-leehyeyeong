d3cd56840fd1f27b4633f5291dd9abc293dca5c8
leehyeyeong <elto789@naver.com>
2023-11-14 14:11:58 +0900

    feat: input, output run 함수 실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import InputView from "./InputView.js";
+import OutputView from "./OutputView.js";
+
 class App {
-  async run() {}
+  async run() {
+    OutputView.printGreet();
+    await InputView.readDate();
+    OutputView.printEventPreview();
+  }
 }
 
 export default App;
