ad65372a2824f418c91939de16c79ae42183766b
leehyeyeong <elto789@naver.com>
2023-11-14 14:11:36 +0900

    feat: 기본적인 시작 입출력 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,12 @@
+import { Console } from "@woowacourse/mission-utils";
 const OutputView = {
+  printGreet() {
+    Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+  },
+  printEventPreview() {
+    Console.print("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    Console.print("")
+  },
   printMenu() {
     Console.print("<주문 메뉴>");
     // ...
