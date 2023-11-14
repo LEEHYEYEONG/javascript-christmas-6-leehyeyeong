a032bf32c90bdcbb90bb207c035c9c86f946846d
leehyeyeong <elto789@naver.com>
2023-11-14 14:11:22 +0900

    feat: 할인 하는 요건 상수로 정리

---
+++ src/Constant.js
@@ -0,0 +1,14 @@
+export const MENU = {
+  APPETIZER: { 양송이수프: 6000, 타파스: 5500, 시저샐러드: 8000 },
+  MAIN: { 티본스테이크: 55000, 바비큐립: 54000, 해산물파스타: 35000, 크리스마스파스타: 25000 },
+  DESSERT: { 초코케이크: 15000, 아이스크림: 5000 },
+  DRINK: { 제로콜라: 3000, 레드와인: 60000, 아이스크림: 25000 }
+}
+
+export const WEEKDAY = [3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 24, 25, 26, 27, 28, 31]
+
+export const WEEKEND = [1, 2, 8, 9, 15, 16, 22, 23, 29, 30]
+
+export const SPECIAL = [3, 10, 17, 24, 25, 31]
+
+export default { MENU, WEEKDAY, WEEKEND, SPECIAL }
