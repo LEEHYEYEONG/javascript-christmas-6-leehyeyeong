11f21bafb7496e216b09f187ba97ecd92ac04fc5
leehyeyeong <elto789@naver.com>
2023-11-14 16:13:13 +0900

    feat: 메뉴 입력 - 예외처리 - 딕셔너리로 정리 - 주문 금액 합계 리턴 함수 작성

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
 import Day from "./Day.js"
+import Menu from "./Menu.js";
 
 const InputView = {
   async readDate() {
@@ -7,10 +8,11 @@ const InputView = {
     const day = new Day(inputDay);
     day.dayValidate();
     const dayDiscount = day.discount();
-    Console.print(dayDiscount);
   },
   async readMenu() {
     const inputMenu = await Console.readLineAsync("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n");
+    const menu = new Menu(inputMenu);
+    menu.menuValidate();
   }
 
   // ...
---
+++ src/Menu.js
@@ -0,0 +1,68 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MENU } from "./Constant.js";
+
+class Menu {
+  constructor(menu) {
+    this.menu = menu.includes(",") ? menu.split(',').flatMap(part => part.split('-')) : menu.split('-');
+    this.menuName = this.menu.filter((_, index) => index % 2 === 0);
+    this.menuDict = this.menu.reduce((acc, cur, i) => (i % 2 === 0 ? { ...acc, [cur]: parseInt(this.menu[i + 1]) } : acc), {});
+  }
+
+  menuExist(key) {
+    if (!Object.values(MENU).some(category => key in category)) {
+      throw new Error("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+  }
+
+  menuNum(key) {
+    if (this.menuDict[key] < 1 || isNaN(this.menuDict[key])) {
+      throw new Error("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+  }
+
+  menuDuplicate() {
+    if (this.menuName.length != new Set(this.menuName).size) {
+      throw new Error("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+  }
+
+  menuAdd(menuSum) {
+    if (menuSum > 20) {
+      throw new Error("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+  }
+
+  drinkAdd(drinkSum) {
+    if (Object.keys(this.menuDict).length === drinkSum) {
+      throw new Error("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+  }
+
+  menuValidate() {
+    let menuSum = 0
+    let drinkSum = 0
+    this.menuDuplicate();
+    for (let key in this.menuDict) {
+      this.menuExist(key);
+      this.menuNum(key);
+      if (key in MENU["DRINK"]) {
+        drinkSum += 1
+      }
+      menuSum += this.menuDict[key]
+    }
+    this.menuAdd(menuSum);
+    this.drinkAdd(drinkSum);
+  }
+
+  returnMenu() {
+    return this.menuDict;
+  }
+
+  returnAmountSum() {
+    let amountSum = 0
+    Object.keys(this.menuDict).forEach((key) => amountSum += this.menuDict[key]);
+    return amountSum;
+  }
+}
+
+export default Menu
