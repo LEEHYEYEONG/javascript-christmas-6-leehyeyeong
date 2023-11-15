39360098defcb7ec6a4b13345bfaf2d573dba6ba
LEEHYEYEONG <elto789@naver.com>
2023-11-15 17:47:42 +0900

    feat: 예외처리 후 재실행

--- src/InputView.js
+++ src/InputView.js
@@ -4,22 +4,32 @@ import Menu from "./Menu.js";
 
 const InputView = {
   async readDate() {
-    const inputDay = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n");
-    const day = new Day(inputDay);
-    day.dayValidate();
-    const dayDiscount = day.discount();
-    return dayDiscount;
+    while (true) {
+      try {
+        const inputDay = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n");
+        const day = new Day(inputDay);
+        day.dayValidate();
+        const dayDiscount = day.discount();
+        return dayDiscount;
+      } catch (error) {
+        Console.print(`${error.message}`);
+      }
+    }
   },
   async readMenu() {
-    const inputMenu = await Console.readLineAsync("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n");
-    const menu = new Menu(inputMenu);
-    menu.menuValidate();
-    const menuDict = menu.returnMenu();
-    const amoutSum = menu.returnAmountSum();
-    return [menuDict, amoutSum]
+    while (true) {
+      try {
+        const inputMenu = await Console.readLineAsync("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n");
+        const menu = new Menu(inputMenu);
+        menu.menuValidate();
+        const menuDict = menu.returnMenu();
+        const amoutSum = menu.returnAmountSum();
+        return [menuDict, amoutSum]
+      } catch (error) {
+        Console.print(`${error.message}`);
+      }
+    }
   }
-
-  // ...
 }
 
 export default InputView;
