2e2d12009325276b89b9b0d563997340d10d8f91
LEEHYEYEONG <elto789@naver.com>
2023-11-15 17:17:45 +0900

    feat: 실행 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
 import InputView from "./InputView.js";
 import OutputView from "./OutputView.js";
 
 class App {
   async run() {
     OutputView.printGreet();
-    await InputView.readDate();
-    await OutputView.printMenu();
-
+    const dayDiscount = await InputView.readDate();
+    const item = await OutputView.printMenu();
+    const totalBenefit = OutputView.printBenefits(dayDiscount, item);
+    OutputView.printTotalBenefits(totalBenefit, item[1][1]);
+    OutputView.printBadge(totalBenefit[0]);
   }
 }
 
